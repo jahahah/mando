50713d4fe5dbfc0c5aa72e59cd775d2cc4f1327d
David Team <79711529+DavidTeam8@users.noreply.github.com>
2021-05-25 04:50:24 +0300

    Update David.lua

--- David.lua
+++ David.lua
@@ -7993,7 +7993,7 @@ local TXTE = "⌁︙اعدادات المجموعه ↫ ⤈\n┉ ≈ ┉ ≈ ┉
 .."⌁︙عدد التكرار ↫ "..Flood_Num.."\n"
 .."⌁︙عدد السبام ↫ "..spam_c.."\n"
 .."┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉\n⌁︙".."\n"
-[Source Channel](https://t.me/L9L9L)\n"
+"[Source Channel](https://t.me/L9L9L)\n"
 Dev_Rio(msg.chat_id_, msg.id_, 1, TXTE, 1, 'md')
 end
 end
